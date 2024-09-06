7853d9a222168659f928b0f340e885fa681ba273
Gregory Gerard <wako057@gmail.com>
2024-09-06 15:22:55 +0200

    feat: add firebase hit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
-
+const FIREBASE_BASE_URL = 'https://react-http-80662-default-rtdb.europe-west1.firebasedatabase.app';
 const showCart = useSelector(state => state.ui.cartVisible);
+const cart = useSelector((state) => state.cart);
+
+useEffect(() => {
+  fetch(
+    `${FIREBASE_BASE_URL}/cart.json`, 
+    { 
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+}, [cart]);
 
   return (
     <Layout>
